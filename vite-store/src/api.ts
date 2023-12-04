df8eafef7256711d83c8bef2ec5039b51bbd6ea1
Eric Bower <me@erock.io>
2023-12-04 09:32:18 -0500

    chore: update starfx

--- vite-store/src/api.ts
+++ vite-store/src/api.ts
@@ -1,4 +1,4 @@
-import { createApi, fetcher, requestMonitor } from "starfx";
+import { createApi, mdw } from "starfx";
 import { takeEvery, storeMdw, slice, createSchema } from 'starfx/store';
 
 interface User {
@@ -9,31 +9,33 @@ interface User {
 const emptyUser = { id: "", name: "" };
 export const schema = createSchema({
   users: slice.table({ empty: emptyUser }),
-  data: slice.table({ empty: {} }),
+  cache: slice.table({}),
   loaders: slice.loader(),
 });
 export type AppState = typeof schema.initialState;
 export const db = schema.db;
 
 export const api = createApi();
-api.use(requestMonitor());
+api.use(mdw.api());
 api.use(storeMdw(db));
 api.use(api.routes());
-api.use(fetcher({ baseUrl: 'https://jsonplaceholder.typicode.com' }));
+api.use(mdw.fetch({ baseUrl: 'https://jsonplaceholder.typicode.com' }));
 
 export const fetchUsers = api.get<never, User[]>(
   '/users',
   { supervisor: takeEvery },
   function*(ctx, next) {
     yield* next();
+
     if (!ctx.json.ok) {
       return;
     }
 
-    const users = ctx.json.data.reduce<Record<string, User>>((acc, user) => {
+    const users = ctx.json.value.reduce<Record<string, User>>((acc, user) => {
       acc[user.id] = user;
       return acc;
     }, {});
+
     yield* schema.update(db.users.add(users));
   },
 );
